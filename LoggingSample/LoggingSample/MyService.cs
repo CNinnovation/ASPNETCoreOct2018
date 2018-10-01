c02d52e28838554077b7c105484c874c789938d7
Christian Nagel <christian@christiannagel.com>
2018-10-01 15:35:32 +0200

    logging

---
+++ LoggingSample/LoggingSample/MyService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoggingSample
+{
+    public class MyService
+    {
+        private readonly ILogger<MyService> _logger;
+        public MyService(ILogger<MyService> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public void Foo(string n)
+        {
+            // _logger.Log(LogLevel.Information, "Foo started", n);
+            _logger.LogInformation("Foo started", n);
+          
+        }
+
+        public void Bar()
+        {
+            _logger.LogError("Some error in Bar");
+        }
+    }
+}
---
+++ LoggingSample/LoggingSample/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using Microsoft.Extensions.Logging;
+
+namespace LoggingSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegisterServices();
+
+            var s = Container.GetService<MyService>();
+            s.Foo("test");
+            s.Bar();
+            Console.WriteLine("Main end");
+            Console.ReadLine();
+        }
+
+        private static void RegisterServices()
+        {
+            var services = new ServiceCollection();
+            services.AddTransient<MyService>();
+            services.AddLogging(config =>
+            {
+                config.AddConsole();
+                config.AddDebug();
+                config.AddFilter(level => true);
+            });
+            Container = services.BuildServiceProvider();
+        }
+
+        public static IServiceProvider Container { get; private set; }
+    }
+}
